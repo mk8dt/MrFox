794dbb1fb672bc6dcf35d105af3d9bc94a99fefd
mario <mario.g.a.90@gmail.com>
2018-03-02 11:53:35 +0100

    commit final

--- app/src/main/java/com/example/mario/mrfox/SplashActivity.java
+++ app/src/main/java/com/example/mario/mrfox/SplashActivity.java
@@ -8,27 +8,34 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
     TextView tvTitulo;
     ImageView iv;
+    RelativeLayout rl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvTitulo=findViewById(R.id.tvSplash);
         iv=findViewById(R.id.ivSplash);
+        rl=findViewById(R.id.rl);
 
         Typeface face=Typeface.createFromAsset(getAssets(),"font/AmaticSC-Regular.ttf");
         tvTitulo.setTypeface(face);
 
-       Animation traslate = AnimationUtils.loadAnimation(this, R.anim.translate);
-       tvTitulo.startAnimation(traslate);
+        Animation traslate = AnimationUtils.loadAnimation(this, R.anim.translate);
+        tvTitulo.startAnimation(traslate);
 
         Animation scale = AnimationUtils.loadAnimation(this, R.anim.scale);
         iv.startAnimation(scale);
+
+        Animation alpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        rl.startAnimation(alpha);
 
         new Handler().postDelayed(new Runnable() {
             @Override
